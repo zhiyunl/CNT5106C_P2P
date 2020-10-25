35365b421d19b7ab8a982b2574ae720bbe98223e
Zhiyun Ling <chihyunling@gmail.com>
2020-10-25 10:18:49 -0400

    use arguments to set peerID

--- src/com/company/impl/Main.java
+++ src/com/company/impl/Main.java
@@ -8,15 +8,15 @@ import java.io.IOException;
 import java.util.List;
 
 public class Main {
-    private static final int id = 1001;
-
-    private Main() {
+    private int id = 1001;
+    private Main(int id) {
+        this.id = id;
         P2PFileProcess p2PFileProcess = new P2PFileProcess();
         try {
             List<P2PFileProcess.PeerInfo> peersinfo = p2PFileProcess.PeerInfoCfg();
-            ServerListener serverListener = new ServerListener(peersinfo, id);
+            ServerListener serverListener = new ServerListener(peersinfo, this.id);
             serverListener.start();
-            ClientListener clientListener = new ClientListener(peersinfo, id);
+            ClientListener clientListener = new ClientListener(peersinfo, this.id);
             clientListener.start();
         } catch (IOException e) {
             e.printStackTrace();
@@ -24,7 +24,13 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        new Main();
+        // Use id as arguments to set peerProcess ID
+        if (args.length >= 1){
+            new Main(Integer.parseInt(args[0]));
+        }else{
+            new Main(1001); // default id is 1001
+        }
+
     }
 
 }
