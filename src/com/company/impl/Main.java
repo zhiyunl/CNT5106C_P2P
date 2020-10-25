fb18cb67c86c7d53e9d38bacf809bc5ad7509f31
k5300 <k530003019@mail.uic.edu.hk>
2020-10-25 18:13:34 +0800

    The first stage of project is completed

--- src/com/company/impl/Main.java
+++ src/com/company/impl/Main.java
@@ -8,11 +8,7 @@ import java.io.IOException;
 import java.util.List;
 
 public class Main {
-
-    private static final int sPort = 8001;   //The server will be listening on this port number
     private static final int id = 1001;
-    private int[] peerList = new int[]{1001, 1002, 1003};
-    private int[] socketList = new int[]{8001, 8002, 8003};
 
     private Main() {
         P2PFileProcess p2PFileProcess = new P2PFileProcess();
