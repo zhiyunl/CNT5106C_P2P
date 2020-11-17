d1ac460b89205d9d064c7b9684d2dd08f88ae8c5
Zhiyun Ling <chihyunling@gmail.com>
2020-11-16 22:55:16 -0500

    Add shell script

---
+++ build.sh
@@ -0,0 +1,15 @@
+find . -name "*.java" > build.txt
+javac -d . @build.txt
+#java com.company.impl.Main 1001 &
+#P1 = $!
+#java com.company.impl.Main 1002 &
+#P2 = $!
+#java com.company.impl.Main 1003 &
+#P3 = $!
+#wait $P1 $P2 $P3
+java com.company.impl.Main 1001 > log1.txt 2>&1 &
+sleep 1
+java com.company.impl.Main 1002 > log2.txt 2>&1 &
+sleep .001
+java com.company.impl.Main 1003 > log3.txt 2>&1 &
+
